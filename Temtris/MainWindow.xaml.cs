4645d3753f30067e27a4e14a3c6d6150f4a999e2
BrightS0le <hunmjohn@ut.utm.edu>
2023-02-04 23:07:58 -0600

    I'm terrified of it breaking after I got half the block collision figured out cause rn one small mistake could break everything and im not about to deal with that so im going to actually use this nifty git feature so i dont have to worry about it cause like right now the dopamine release just from getting this to work is crazy i cant believe i actually got this to work, like holy moly, who cares if it might be inefficient or be hard to read and totally not follow and sort of modular use. IT. FREAKING. WORKS. BABY. HELL. YEAH.

--- Temtris/MainWindow.xaml.cs
+++ Temtris/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.ComponentModel;
 
 namespace Temtris
 {
@@ -23,6 +25,92 @@ namespace Temtris
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private bool[,] blocks = new bool[20, 10];
+        private int row = 0;
+        private int column = 5;
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(255, 0, 100, 0));
+            Rectangle temp = new Rectangle();
+            Grid.SetRow(temp, row);
+            Grid.SetColumn(temp, column);
+            temp.Width = 54;
+            temp.Height = 54;
+            temp.Fill = brush;
+            temp.StrokeThickness = 5;
+            temp.Stroke = brush;
+            MainMenu_Grid.Children.Add(temp);
+            Movement(temp);
+        }
+        private void Movement(Rectangle test)
+        {
+            if (Checker(test) == false)
+            {
+                var backgroundWorker = new BackgroundWorker();
+                backgroundWorker.DoWork += (s, v) => { Thread.Sleep(500); };
+                backgroundWorker.RunWorkerCompleted += (s, v) =>
+                {
+                    Grid.SetRow(test, Grid.GetRow(test) + 1);
+                    if (Grid.GetRow(test) < 18)
+                    {
+                        if (Keyboard.IsKeyDown(Key.A) || Keyboard.IsKeyDown(Key.D))
+                        {
+                            if (Keyboard.IsKeyDown(Key.A))
+                            {
+                                Movement_2(true, test);
+                            }
+                            else
+                            {
+                                Movement_2(false, test);
+                            }
+                        }
+                        Movement(test);
+                    }
+                    else
+                    {
+                        blocks[Grid.GetRow(test), (Grid.GetColumn(test) - 1)] = true;
+                    }
+                };
+                backgroundWorker.RunWorkerAsync();
+            }
+            else
+            {
+                blocks[Grid.GetRow(test), Grid.GetColumn(test) - 1] = true;
+            }
+        }
+        private void Movement_2(bool direction, Rectangle temp)
+        {
+            if (Grid.GetColumn(temp) >= 1 && Grid.GetColumn(temp) <= 10)
+            {
+                if (direction == true && Grid.GetColumn(temp) != 1)
+                {
+                    Grid.SetColumn(temp, Grid.GetColumn(temp) - 1);
+                }
+                else if(direction == false && Grid.GetColumn(temp) != 10)
+                {
+                    Grid.SetColumn(temp, Grid.GetColumn(temp) + 1);
+                }
+            }
+        }
+        private bool Checker(Rectangle test)
+        {
+            if (Grid.GetRow(test) + 1 < 20)
+            {
+                if (blocks[Grid.GetRow(test) + 1, (Grid.GetColumn(test) - 1)] == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
