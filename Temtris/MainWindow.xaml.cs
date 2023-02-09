18df8422af8d558d99151bb5c2c7a9973027a391
BrightS0le <hunmjohn@ut.utm.edu>
2023-02-09 15:23:44 -0600

    Left right collision tested and working for a single block

--- Temtris/MainWindow.xaml.cs
+++ Temtris/MainWindow.xaml.cs
@@ -14,6 +14,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 using System.ComponentModel;
+using System.Diagnostics;
+
+
+
+
 
 namespace Temtris
 {
@@ -26,29 +31,67 @@ namespace Temtris
         {
             InitializeComponent();
         }
-        private bool[,] blocks = new bool[20, 10];
-        private int row = 0;
-        private int column = 5;
+        private bool[,] blocks = new bool[25, 15];
+        private int test_gg = 0;
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
             SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(255, 0, 100, 0));
             Rectangle temp = new Rectangle();
-            Grid.SetRow(temp, row);
-            Grid.SetColumn(temp, column);
+            Grid.SetRow(temp, 0);
+            Grid.SetColumn(temp, 4);
             temp.Width = 54;
             temp.Height = 54;
             temp.Fill = brush;
             temp.StrokeThickness = 5;
             temp.Stroke = brush;
-            MainMenu_Grid.Children.Add(temp);
+            if (test_gg == 0)
+            {
+                createGameBoard();
+                test_gg++;
+            }
+            GameGrid.Children.Add(temp);
             Movement(temp);
         }
+        private Grid GameGrid;
+        private void createGameBoard()
+        {
+            GameGrid = new Grid();
+            GameGrid.ShowGridLines = true;
+            RowDefinition row = new RowDefinition();
+            row.Height = new GridLength(54, GridUnitType.Pixel);
+            for (int i = 1; i <= 20; i++)
+            {
+                GameGrid.RowDefinitions.Add(row);
+                row = new RowDefinition();
+                row.Height = new GridLength(54, GridUnitType.Pixel);
+            };
+            ColumnDefinition col = new ColumnDefinition();
+            col.Width = new GridLength(54, GridUnitType.Pixel);
+            for(int i = 1; i <= 10; i++)
+            {
+                GameGrid.ColumnDefinitions.Add(col);
+                col = new ColumnDefinition();
+                col.Width = new GridLength(54, GridUnitType.Pixel);
+            }
+
+            Grid.SetRow(GameGrid, 0);
+            Grid.SetColumn(GameGrid, 1);
+            Grid.SetRowSpan(GameGrid, 20);
+            Grid.SetColumnSpan(GameGrid, 10);
+            MainMenu_Grid.Children.Add(GameGrid);
+        }   
         private void Movement(Rectangle test)
         {
+            
             if (Checker(test) == false)
             {
                 var backgroundWorker = new BackgroundWorker();
-                backgroundWorker.DoWork += (s, v) => { Thread.Sleep(500); };
+                backgroundWorker.DoWork += (s, v) => {
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    while (sw.Elapsed.Milliseconds <= 500) { };
+                    sw.Stop(); 
+                };
                 backgroundWorker.RunWorkerCompleted += (s, v) =>
                 {
                     Grid.SetRow(test, Grid.GetRow(test) + 1);
@@ -69,25 +112,25 @@ namespace Temtris
                     }
                     else
                     {
-                        blocks[Grid.GetRow(test), (Grid.GetColumn(test) - 1)] = true;
+                        blocks[Grid.GetRow(test), Grid.GetColumn(test)] = true;
                     }
                 };
                 backgroundWorker.RunWorkerAsync();
             }
             else
             {
-                blocks[Grid.GetRow(test), Grid.GetColumn(test) - 1] = true;
+                blocks[Grid.GetRow(test), Grid.GetColumn(test)] = true;
             }
         }
         private void Movement_2(bool direction, Rectangle temp)
         {
-            if (Grid.GetColumn(temp) >= 1 && Grid.GetColumn(temp) <= 10)
+            if (Grid.GetColumn(temp) >= 0 && Grid.GetColumn(temp) <= 10)
             {
-                if (direction == true && Grid.GetColumn(temp) != 1)
+                if (direction == true && Grid.GetColumn(temp) != 0 && blocks[Grid.GetRow(temp), Grid.GetColumn(temp) - 1] == false)
                 {
                     Grid.SetColumn(temp, Grid.GetColumn(temp) - 1);
                 }
-                else if(direction == false && Grid.GetColumn(temp) != 10)
+                else if(direction == false && Grid.GetColumn(temp) != 10 && blocks[Grid.GetRow(temp), Grid.GetColumn(temp) + 1] == false)
                 {
                     Grid.SetColumn(temp, Grid.GetColumn(temp) + 1);
                 }
@@ -97,7 +140,7 @@ namespace Temtris
         {
             if (Grid.GetRow(test) + 1 < 20)
             {
-                if (blocks[Grid.GetRow(test) + 1, (Grid.GetColumn(test) - 1)] == true)
+                if (blocks[Grid.GetRow(test) + 1, Grid.GetColumn(test)] == true)
                 {
                     return true;
                 }
