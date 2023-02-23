f333d06cb1c4dcd679ba5996bfbfc297cb76129a
Curt Lynch <curjlync@ut.utm.edu>
2023-02-23 13:23:24 -0600

    Outline using uml

---
+++ Temtris/GameEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Temtris
+{
+    abstract class GameEngine
+    {
+        public void Start()
+        {
+
+        }
+
+        private void GameLoop()
+        {
+
+        }
+
+        abstract protected void OnStart();
+
+        abstract protected void OnUpdate(float ElapsedTime);
+
+        
+    }
+}
---
+++ Temtris/Matrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Temtris
+{
+    enum Color
+    {
+        Red, Green, Blue, Pink;
+    }
+
+    struct mino
+    {
+        
+        public int x,y;
+        public Color color;
+    }
+
+    internal class Matrix
+    {
+        private List<mino> minos_active;
+        private List<mino> minos_inactive;
+
+        public void Update(float ElapsedTime)
+        {
+
+        }
+    }
+}
